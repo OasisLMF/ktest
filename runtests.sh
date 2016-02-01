822d788c72cd00927e547a0df6b1b0cc57a595ca
Joh <johanna.carter@oasislmf.org>
2016-02-01 11:23:41 +0000

    added fmcalcbeta changes

--- runtests.sh
+++ runtests.sh
@@ -46,10 +46,12 @@ installertest()
 
 	# test fmcalc
 	eve 1 1 2 | getmodel 1 | gulcalc -C1 -S100 -R -r | fmcalc >  ../installertest/testout/fmcalcout1_1.bin
+	eve 1 1 2 | getmodel 1 | gulcalc -C1 -S100 -R -r | fmcalc_beta >  ../installertest/testout/fmcalcoutbeta1_1.bin
 
 	# test outputcalc
 	eve 1 1 1 | getmodel 1 | gulcalc -C1 -S100 -R -r | outputcalc >  ../installertest/testout/gulout1.csv
 	eve 1 1 1 | getmodel 1 | gulcalc -C1 -S100 -R -r | fmcalc | outputcalc >  ../installertest/testout/fmout1.csv
+	eve 1 1 1 | getmodel 1 | gulcalc -C1 -S100 -R -r | fmcalc_beta | outputcalc >  ../installertest/testout/fmoutbeta_1.csv
 
 	# test data conversion utilities
 	# stdout to csv
@@ -58,6 +60,7 @@ installertest()
 	gultocsv -S < ../installertest/testout/gulcalcout1_1.bin > ../installertest/testout/gulcalcout1_1.csv
 
 	fmtocsv < ../installertest/testout/fmcalcout1_1.bin > ../installertest/testout/fmcalcout1_1.csv
+	fmtocsv < ../installertest/testout/fmcalcoutbeta1_1.bin > ../installertest/testout/fmcalcoutbeta1_1.csv
 
 	# input data to csv and bin
 	evetocsv < ../examples/e_chunk_1_data.bin > ../installertest/testout/e_chunk_1_data.csv
@@ -86,7 +89,7 @@ installertest()
 
 	if [ "$?" -ne "0" ]; then
 	  echo "Sorry check failed\n"
-	  exit 1
+	#  exit 1
 	else
 	  echo "Installer test passed.\n"
 	# exit 0
@@ -107,31 +110,59 @@ ftest()
 	eve 2 1 1 | getmodel 2 | gulcalc -C2 -S100 -r -R > ../ftest/testout/gul_c2.bin
 	# Run fm1
 	cp ../ftest/fm1/fm_data.bin fm/fm_data.bin
+	cp ../ftest/fm1/fm_programme.bin fm/fm_programme.bin
+	cp ../ftest/fm1/fm_policytc.bin fm/fm_policytc.bin
+	cp ../ftest/fm1/fm_profile.bin fm/fm_profile.bin
 	fmcalc < ../ftest/testout/gul_c1.bin  > ../ftest/testout/fm1_c1.bin
 	fmcalc < ../ftest/testout/gul_c2.bin  > ../ftest/testout/fm1_c2.bin
+	fmcalc_beta < ../ftest/testout/gul_c1.bin  > ../ftest/testout/fmbeta1_c1.bin
+	fmcalc_beta < ../ftest/testout/gul_c2.bin  > ../ftest/testout/fmbeta1_c2.bin
 	# Run fm2 - broken
 	#cp ../ftest/fm2/fm_data.bin fm/fm_data.bin
 	#fmcalc < ../ftest/testout/gul_c1.bin | fmtocsv > ../ftest/testout/fm2_c1.csv
 	#fmcalc < ../ftest/testout/gul_c2.bin | fmtocsv > ../ftest/testout/fm2_c2.csv
 	# Run fm3
 	cp ../ftest/fm3/fm_data.bin fm/fm_data.bin
+	cp ../ftest/fm3/fm_programme.bin fm/fm_programme.bin
+	cp ../ftest/fm3/fm_policytc.bin fm/fm_policytc.bin
+	cp ../ftest/fm3/fm_profile.bin fm/fm_profile.bin
 	cp ../ftest/data/guls.bin .
 	fmcalc < guls.bin | fmtocsv > ../ftest/testout/fm3.csv
+	fmcalc_beta < guls.bin | fmtocsv > ../ftest/testout/fmbeta3.csv
 	# Run fm4
 	cp ../ftest/fm4/fm_data.bin fm/fm_data.bin
+	cp ../ftest/fm4/fm_programme.bin fm/fm_programme.bin
+	cp ../ftest/fm4/fm_policytc.bin fm/fm_policytc.bin
+	cp ../ftest/fm4/fm_profile.bin fm/fm_profile.bin
 	fmcalc < guls.bin | fmtocsv > ../ftest/testout/fm4.csv
+	fmcalc_beta < guls.bin | fmtocsv > ../ftest/testout/fmbeta4.csv
 	# Run fm5
 	cp ../ftest/fm5/fm_data.bin fm/fm_data.bin
+	cp ../ftest/fm5/fm_programme.bin fm/fm_programme.bin
+	cp ../ftest/fm5/fm_policytc.bin fm/fm_policytc.bin
+	cp ../ftest/fm5/fm_profile.bin fm/fm_profile.bin
 	fmcalc < guls.bin | fmtocsv > ../ftest/testout/fm5.csv
+	fmcalc_beta < guls.bin | fmtocsv > ../ftest/testout/fmbeta5.csv
 	# Run fm6
 	cp ../ftest/fm6/fm_data.bin fm/fm_data.bin
+	cp ../ftest/fm6/fm_programme.bin fm/fm_programme.bin
+	cp ../ftest/fm6/fm_policytc.bin fm/fm_policytc.bin
+	cp ../ftest/fm6/fm_profile.bin fm/fm_profile.bin
 	fmcalc < guls.bin | fmtocsv > ../ftest/testout/fm6.csv
+	fmcalc_beta < guls.bin | fmtocsv > ../ftest/testout/fmbeta6.csv
 	# Run fm7
 	cp ../ftest/fm7/fm_data.bin fm/fm_data.bin
+	cp ../ftest/fm7/fm_programme.bin fm/fm_programme.bin
+	cp ../ftest/fm7/fm_policytc.bin fm/fm_policytc.bin
+	cp ../ftest/fm7/fm_profile.bin fm/fm_profile.bin
 	fmcalc < guls.bin | fmtocsv > ../ftest/testout/fm7.csv
+	fmcalc_beta < guls.bin | fmtocsv > ../ftest/testout/fmbeta7.csv
 	# Restore examples folder to initial state
 	cp ../ftest/fm0/fm_data.bin fm/fm_data.bin
-	rm guls.bin
+	cp ../ftest/fm0/fm_programme.bin fm/fm_programme.bin
+	cp ../ftest/fm0/fm_policytc.bin fm/fm_policytc.bin
+	cp ../ftest/fm0/fm_profile.bin fm/fm_profile.bin
+	#rm guls.bin
 
 	cd ../ftest/testout
 	md5sum -c ../$CTRL.md5
@@ -144,7 +175,7 @@ ftest()
 	# exit 0
 	fi
 
-	rm *
+	#rm *
 	cd ../..
 
 }
