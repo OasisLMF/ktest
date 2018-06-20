b08d49246b8ed132491eb3b5d25fa3c3eb12951e
Joh <johanna.carter@oasislmf.org>
2018-06-20 11:05:51 +0100

    updated to new format

--- ftest/runtests.sh
+++ ftest/runtests.sh
@@ -12,12 +12,12 @@ main()
 	fmcalc < ../testout/gul_c2.bin | fmtocsv -f > ../testout/fm0_c2.csv
 	# Run fm1
 	cd ../fm1
-	fmcalc < ../testout/gul_c1.bin  > ../testout/fm1_c1.bin
-	fmcalc < ../testout/gul_c2.bin  > ../testout/fm1_c2.bin
+	fmcalc -a1 < ../testout/gul_c1.bin  > ../testout/fm1_c1.bin
+	fmcalc -a1 < ../testout/gul_c2.bin  > ../testout/fm1_c2.bin
 	# Run fm2 
 	cd ../fm2
-	fmcalc < ../testout/gul_c1.bin  > ../testout/fm2_c1.bin
-	fmcalc < ../testout/gul_c2.bin  > ../testout/fm2_c2.bin
+	fmcalc -a1 < ../testout/gul_c1.bin  > ../testout/fm2_c1.bin
+	fmcalc -a1 < ../testout/gul_c2.bin  > ../testout/fm2_c2.bin
 	# Run fm3
 	cd ../fm3
 	fmcalc < stream/gulitem.bin | fmtocsv -f > ../testout/fm3.csv
@@ -62,44 +62,45 @@ main()
 	fmcalc < stream/gulitem.bin | fmtocsv -f > ../testout/fm16.csv
 	# Run fm17
 	cd ../fm17
-	fmcalc < stream/gulitem.bin | fmtocsv -f > ../testout/fm17.csv
+	fmcalc -a1 < stream/gulitem.bin | fmtocsv -f > ../testout/fm17.csv
 	# Run fm18
 	cd ../fm18
-	fmcalc < stream/gulitem.bin | fmtocsv -f > ../testout/fm18.csv
+	fmcalc -a2 < stream/gulitem.bin | fmtocsv -f > ../testout/fm18.csv
 	# Run fm19
 	cd ../fm19
-	fmcalc < stream/gulitem.bin | fmtocsv -f > ../testout/fm19.csv
+	fmcalc -a2 < stream/gulitem.bin | fmtocsv -f > ../testout/fm19.csv
 	# Run fm20
 	cd ../fm20
-	fmcalc < stream/gulitem.bin | fmtocsv -f > ../testout/fm20.csv
+	fmcalc -a1 < stream/gulitem.bin | fmtocsv -f > ../testout/fm20.csv
 	# Run fm21
 	cd ../fm21
-	fmcalc < stream/gulitem.bin | fmtocsv -f > ../testout/fm21.csv
+	fmcalc -a2 < stream/gulitem.bin | fmtocsv -f > ../testout/fm21.csv
 	# Run fm22
 	cd ../fm22
-	fmcalc < stream/gulitem.bin | fmtocsv -f > ../testout/fm22.csv
+	fmcalc -a2 < stream/gulitem.bin | fmtocsv -f > ../testout/fm22.csv
     # Run fm23
 	cd ../fm23
-	fmcalc -p direct < stream/gulitem.bin | tee ../testout/fm23_ils.bin | fmtocsv > ../testout/fm23_ils.csv
-  	fmcalc -p ri1 -n < ../testout/fm23_ils.bin | tee ../testout/fm23_ri1.bin | fmtocsv > ../testout/fm23_ri1.csv
-	fmcalc -p ri2 -n < ../testout/fm23_ri1.bin | fmtocsv > ../testout/fm23_ri2.csv
+	fmcalc -a1 -p direct < stream/gulitem.bin | tee ../testout/fm23_ils.bin | fmtocsv > ../testout/fm23_ils.csv
+  	fmcalc -a1 -p ri1 -n < ../testout/fm23_ils.bin | tee ../testout/fm23_ri1.bin | fmtocsv > ../testout/fm23_ri1.csv
+	fmcalc -a1 -p ri2 -n < ../testout/fm23_ri1.bin | fmtocsv > ../testout/fm23_ri2.csv
     # Run fm24
 	cd ../fm24
-	fmcalc < stream/gulitem.bin | tee ../testout/fm24_ils.bin | fmtocsv > ../testout/fm24_ils.csv
-  	fmcalc -p ri1 -n < ../testout/fm24_ils.bin | tee ../testout/fm24_ri1.bin | fmtocsv > ../testout/fm24_ri1.csv
-	fmcalc -p ri2 -n < ../testout/fm24_ri1.bin | tee ../testout/fm24_ri2.bin | fmtocsv > ../testout/fm24_ri2.csv
-	fmcalc -p ri3 -n < ../testout/fm24_ri2.bin | tee ../testout/fm24_ri3.bin | fmtocsv > ../testout/fm24_ri3.csv
-	#fmcalc -p ri4 -n < ../testout/fm24_ri3.bin | tee ../testout/fm24_ri4.bin | fmtocsv > ../testout/fm24_ri4.csv
+	fmcalc -a2 < stream/gulitem.bin | tee ../testout/fm24_ils.bin | fmtocsv > ../testout/fm24_ils.csv
+  	fmcalc -a0 -p ri1 -n < ../testout/fm24_ils.bin | tee ../testout/fm24_ri1.bin | fmtocsv > ../testout/fm24_ri1.csv
+	fmcalc -a2 -p ri2 -n < ../testout/fm24_ri1.bin | tee ../testout/fm24_ri2.bin | fmtocsv > ../testout/fm24_ri2.csv
+	fmcalc -a2 -p ri3 -n < ../testout/fm24_ri2.bin | tee ../testout/fm24_ri3.bin | fmtocsv > ../testout/fm24_ri3.csv
+	#fmcalc -a2 -p ri4 < ../testout/fm24_ri3.bin | tee ../testout/fm24_ri4.bin | fmtocsv > ../testout/fm24_ri4_gross.csv
+	fmcalc -a2 -p ri4 -n < ../testout/fm24_ri3.bin | tee ../testout/fm24_ri4.bin | fmtocsv > ../testout/fm24.csv
 	cd ../fm25
 	fmcalc < stream/gulitem.bin | tee ../testout/fm25_ils.bin | fmtocsv > ../testout/fm25_ils.csv
 	cd ../fm26
-	fmcalc -p ri3 < stream/fm24_ri2.bin  | fmtocsv > ../testout/fm26.csv
-	fmcalc -p ri3_net -n < stream/fm24_ri2.bin  | fmtocsv > ../testout/fm26_net.csv
+	fmcalc -a2 -p ri3 < stream/fm24_ri2.bin  | fmtocsv > ../testout/fm26.csv
+	fmcalc -a2 -p ri3_net -n < stream/fm24_ri2.bin  | fmtocsv > ../testout/fm26_net.csv
   	cd ../fm27
 	fmcalc -p iteration1 < stream/fm24_ri2.bin  | tee ../testout/fm27_it1.bin | fmtocsv > ../testout/fm27_it1.csv
   	fmcalc -p iteration2 < ../testout/fm27_it1.bin  | fmtocsv > ../testout/fm27_it2.csv
 	cd ../fm28
-	fmcalc < stream/gulitem.bin | fmtocsv -f > ../testout/fm28.csv
+	fmcalc -a2 < stream/gulitem.bin | fmtocsv -f > ../testout/fm28.csv
 
   	cd ../testout
 	md5sum -c ../$CTRL.md5
