03349a7dafd423cc6cb550b43148d325a455507e
Joh <johanna.carter@oasislmf.org>
2018-07-26 14:28:24 +0100

    updated to output debug data

--- aaltest/runtests.sh
+++ aaltest/runtests.sh
@@ -8,7 +8,7 @@ main()
 
 
 	mkdir -p work/gul1/aal
-
+	mkdir -p work/gul1/summarycalc
 	mkdir -p testout
 
 	# test eve
@@ -19,20 +19,25 @@ main()
 	# test aalcalc
 	eve 1 2 | getmodel | gulcalc -S100 -L0.1 -r -c - | summarycalc -g -1 - | aalcalc > testout/gulaalcalcp1.bin
 	eve 2 2 | getmodel | gulcalc -S100 -L0.1 -r -c - | summarycalc -g -1 - | aalcalc > testout/gulaalcalcp2.bin
-
+	eve 1 2 | getmodel | gulcalc -S100 -L0.1 -r -c - | summarycalc -g -1 testout/gulsummarycalcp1.bin
+	eve 2 2 | getmodel | gulcalc -S100 -L0.1 -r -c - | summarycalc -g -1 testout/gulsummarycalcp2.bin
+	
 	# test aalsummary
 	cp testout/gulaalcalcp1.bin work/gul1/aal/gulaalcalcp1.bin
 	cp testout/gulaalcalcp2.bin work/gul1/aal/gulaalcalcp2.bin
+	cp testout/gulsummarycalcp1.bin work/gul1/summarycalc/p1.bin
+	cp testout/gulsummarycalcp2.bin work/gul1/summarycalc/p2.bin
 
 	aalsummary -Kgul1/aal > testout/gulaalsummary1.csv
-
+	xaalcalc -d -Kgul1/summarycalc > testout/gulxaalcalc1_debug.csv
+	xaalcalc -Kgul1/summarycalc > testout/gulxaalcalc1.csv
 
 	aalcalctocsv < testout/gulaalcalcp1.bin > testout/gulaalcalcp1.csv
 	aalcalctocsv < testout/gulaalcalcp2.bin > testout/gulaalcalcp2.csv
 	
 	cd testout
 	
-	md5sum -c ../$CTRL.md5
+	#md5sum -c ../$CTRL.md5
 
 	if [ "$?" -ne "0" ]; then
 	  echo "Sorry check failed\n"
