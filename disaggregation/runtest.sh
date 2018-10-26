63eb18c7b443b5f407ddadfba09de95eb5ec3630
Joh <johanna.carter@oasislmf.org>
2018-10-26 15:44:53 +0100

    added tests

--- disaggregation/runtest.sh
+++ disaggregation/runtest.sh
@@ -16,6 +16,33 @@ cd ..
 cd d4
 eve 1 1 | getmodel -d | cdftocsv > ../testout/getmodel_$(basename "$PWD").csv
 cd ..
+cd d5
+eve 1 1 | getmodel -d | cdftocsv > ../testout/getmodel_$(basename "$PWD").csv
+cd ..
+cd d6
+eve 1 1 | getmodel -d | cdftocsv > ../testout/getmodel_$(basename "$PWD").csv
+cd ..
+cd d7
+eve 1 1 | getmodel -d | cdftocsv > ../testout/getmodel_$(basename "$PWD").csv
+cd ..
+cd d8
+eve 1 1 | getmodel -d | cdftocsv > ../testout/getmodel_$(basename "$PWD").csv
+cd ..
+cd d9
+eve 1 1 | getmodel -d | cdftocsv > ../testout/getmodel_$(basename "$PWD").csv
+cd ..
+cd d10
+eve 1 1 | getmodel -d | cdftocsv > ../testout/getmodel_$(basename "$PWD").csv
+cd ..
+cd d11
+eve 1 1 | getmodel -d | cdftocsv > ../testout/getmodel_$(basename "$PWD").csv
+cd ..
+cd d12
+eve 1 1 | getmodel -d | cdftocsv > ../testout/getmodel_$(basename "$PWD").csv
+cd ..
+cd d13
+eve 1 1 | getmodel -d | cdftocsv > ../testout/getmodel_$(basename "$PWD").csv
+cd ..
 cd testout
 sha1sum -c ../$CTRL.sha1
 	 if [ "$?" -ne "0" ]; then
