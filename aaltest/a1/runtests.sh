6db28f4f0c0fc3553dc2a5682e2663a5c660d42e
joh <johanna.carter@oasislmf.org>
2019-05-03 12:35:09 +0100

    added aalcalc test a1

--- aaltest/a1/runtests.sh
+++ aaltest/a1/runtests.sh
@@ -7,43 +7,32 @@ main()
 	CTRL=ctrl
 
 
-	mkdir -p work/gul1/aal
 	mkdir -p work/gul1/summarycalc
 	mkdir -p testout
 
-	# test eve
-	eve 1 2 > testout/eveout1.bin
-	eve 2 2 > testout/eveout2.bin
+	
+	# output summarycalc
 
+	eve -n 1 2 | getmodel | gulcalc -S100 -L0.1 -r -c - | summarycalc -g -1 testout/gulsummarycalcp1.bin
+	eve -n 2 2 | getmodel | gulcalc -S100 -L0.1 -r -c - | summarycalc -g -1 testout/gulsummarycalcp2.bin
 	
 	# test aalcalc
-	eve 1 2 | getmodel | gulcalc -S100 -L0.1 -r -c - | summarycalc -g -1 - | aalcalc > testout/gulaalcalcp1.bin
-	eve 2 2 | getmodel | gulcalc -S100 -L0.1 -r -c - | summarycalc -g -1 - | aalcalc > testout/gulaalcalcp2.bin
-	eve 1 2 | getmodel | gulcalc -S100 -L0.1 -r -c - | summarycalc -g -1 testout/gulsummarycalcp1.bin
-	eve 2 2 | getmodel | gulcalc -S100 -L0.1 -r -c - | summarycalc -g -1 testout/gulsummarycalcp2.bin
-	
-	# test aalsummary
-	cp testout/gulaalcalcp1.bin work/gul1/aal/gulaalcalcp1.bin
-	cp testout/gulaalcalcp2.bin work/gul1/aal/gulaalcalcp2.bin
+
 	cp testout/gulsummarycalcp1.bin work/gul1/summarycalc/p1.bin
 	cp testout/gulsummarycalcp2.bin work/gul1/summarycalc/p2.bin
 
-	aalsummary -Kgul1/aal > testout/gulaalsummary1.csv
-	xaalcalc -d -Kgul1/summarycalc > testout/gulxaalcalc1_debug.csv
-	xaalcalc -Kgul1/summarycalc > testout/gulxaalcalc1.csv
-
-	aalcalctocsv < testout/gulaalcalcp1.bin > testout/gulaalcalcp1.csv
-	aalcalctocsv < testout/gulaalcalcp2.bin > testout/gulaalcalcp2.csv
+	aalcalc -Kgul1/summarycalc > testout/gulaal1.csv
+	#aalcalc -x -Kgul1/summarycalc > testout/gulaalx1.csv
 	
 	cd testout
 	
-	#md5sum -c ../$CTRL.md5
+	md5sum -c ../$CTRL.md5
 
 	if [ "$?" -ne "0" ]; then
 	  echo "Sorry check failed\n"
 	#  exit 1
 	else
-	  echo "Installer test passed.\n"
+	  echo "aalcalc test passed.\n"
 	# exit 0
 	fi
 	#rm *
