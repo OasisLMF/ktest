ba17dffc7a56090eb203eed07c0690a6c873d868
Joh <johanna.carter@oasislmf.org>
2019-05-14 11:22:37 +0100

    added a3

--- aaltest/runtests.sh
+++ aaltest/runtests.sh
@@ -27,6 +27,22 @@ main()
 	aalcalc -Kgul1/summarycalc > ../testout/a2_gulaal1.csv
 	#aalcalc -x -Kgul1/summarycalc > testout/gulaalx1.csv	
 	
+	# Test 3 (same as 2 but over 8 cores)
+	cd ../a3
+	mkdir -p work/gul1/summarycalc
+
+	# test aalcalc
+	eve 1 8 | getmodel | gulcalc -S100 -L0.1 -r -c - | summarycalc -g -1 work/gul1/summarycalc/p1.bin
+	eve 2 8 | getmodel | gulcalc -S100 -L0.1 -r -c - | summarycalc -g -1 work/gul1/summarycalc/p2.bin
+	eve 3 8 | getmodel | gulcalc -S100 -L0.1 -r -c - | summarycalc -g -1 work/gul1/summarycalc/p3.bin
+	eve 4 8 | getmodel | gulcalc -S100 -L0.1 -r -c - | summarycalc -g -1 work/gul1/summarycalc/p4.bin
+	eve 5 8 | getmodel | gulcalc -S100 -L0.1 -r -c - | summarycalc -g -1 work/gul1/summarycalc/p5.bin
+	eve 6 8 | getmodel | gulcalc -S100 -L0.1 -r -c - | summarycalc -g -1 work/gul1/summarycalc/p6.bin
+	eve 7 8 | getmodel | gulcalc -S100 -L0.1 -r -c - | summarycalc -g -1 work/gul1/summarycalc/p7.bin
+	eve 8 8 | getmodel | gulcalc -S100 -L0.1 -r -c - | summarycalc -g -1 work/gul1/summarycalc/p8.bin
+	
+	aalcalc -Kgul1/summarycalc > ../testout/a3_gulaal1.csv
+
 	cd ../testout
 	
 	md5sum -c ../$CTRL.md5
