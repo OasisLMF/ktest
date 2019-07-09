05c9cf4a48428948953e807b72f1725a0f2c7255
hchagani-oasislmf <hassan.chagani@oasislmf.org>
2019-07-09 16:05:18 +0100

    Initial commit; validation tests for damagebintobin

---
+++ validationtest/runtests.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+main()
+{
+	mkdir testout
+	CTRL=ctrl
+
+	# Loop over all csv files in static directory and create binary (bin)
+	# and stderr (err) files
+	for filename in static/*.csv; do
+		base=$(basename -s .csv "$filename")
+		damagebintobin < static/$base.csv > testout/$base.bin 2>testout/$base.err
+	done
+
+	# Conduct MD5 checksum on generated files
+	cd testout
+	md5sum -c ../$CTRL.md5
+
+	if [ "$?" -ne "0" ]; then
+		echo "Sorry check failed."
+	else
+		echo "Validation tests passed."
+	fi
+
+	cd ../
+}
+
+main
