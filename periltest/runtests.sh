4d86d8ac1b4eb72edc4fb6ff5e32f46984f32305
joh <johanna.carter@oasislmf.org>
2019-07-25 11:45:01 +0100

    completed test

--- periltest/runtests.sh
+++ periltest/runtests.sh
@@ -9,22 +9,22 @@ main()
 	mkdir -p testout
 
 	# Test subperil allocation method a1 - Sum and cap to TIV and back-allocate to items in proportion
-	eve 1 1 | getmodel | gulcalc -m1 -S10 -L0.1 -r -i - | gultocsv > testout/gulcalci_a1.csv
+	eve 1 1 | getmodel | gulcalc -a1 -S10 -L0.1 -r -i - | gultocsv > testout/gulcalci_a1.csv
 
-	eve 1 1 | getmodel | gulcalc -m1 -S10 -L0.1 -r -i testout/gulitem_a1.bin
-	summarycalc -i -1 testout/summarycalc_S1_a1.bin < testout/gulitem_a1.bin
-	summarycalctocsv < testout/summarycalc_S1_a1.bin > testout/summarycalc_S1_a1.csv
+	# eve 1 1 | getmodel | gulcalc -a1 -S10 -L0.1 -r -i testout/gulitem_a1.bin
+	# summarycalc -i -1 testout/summarycalc_S1_a1.bin < testout/gulitem_a1.bin
+	# summarycalctocsv < testout/summarycalc_S1_a1.bin > testout/summarycalc_S1_a1.csv
+	# summarycalctocsv -e < testout/summarycalc_S1_a1.bin > testout/gulsummarycalc_S1_a1_e.csv
+	# summarycalc -i -2 testout/summarycalc_S2_a1.bin < testout/gulitem_a1.bin
+	# summarycalctocsv < testout/summarycalc_S2_a1.bin > testout/summarycalc_S2_a1.csv
+	# summarycalctocsv -e < testout/summarycalc_S2_a1.bin > testout/gulsummarycalc_S2_a1_e.csv
+	# summarycalc -i -3 testout/summarycalc_S3_a1.bin < testout/gulitem_a1.bin
+	# summarycalctocsv < testout/summarycalc_S3_a1.bin > testout/summarycalc_S3_a1.csv
+	# summarycalctocsv -e < testout/summarycalc_S3_a1.bin > testout/gulsummarycalc_S3_a1_e.csv
+	eve 1 1 | getmodel | gulcalc -a1 -S10 -L0.1 -r -i - | summarycalc -i -1 testout/summarycalc_S1_a1.bin -2 testout/summarycalc_S2_a1.bin -3 testout/summarycalc_S3_a1.bin
 	summarycalctocsv -e < testout/summarycalc_S1_a1.bin > testout/gulsummarycalc_S1_a1_e.csv
-	summarycalc -i -2 testout/summarycalc_S2_a1.bin < testout/gulitem_a1.bin
-	summarycalctocsv < testout/summarycalc_S2_a1.bin > testout/summarycalc_S2_a1.csv
 	summarycalctocsv -e < testout/summarycalc_S2_a1.bin > testout/gulsummarycalc_S2_a1_e.csv
-	summarycalc -i -3 testout/summarycalc_S3_a1.bin < testout/gulitem_a1.bin
-	summarycalctocsv < testout/summarycalc_S3_a1.bin > testout/summarycalc_S3_a1.csv
 	summarycalctocsv -e < testout/summarycalc_S3_a1.bin > testout/gulsummarycalc_S3_a1_e.csv
-	#eve 1 1 | getmodel | gulcalc -m1 -S10 -L0.1 -r -i - | summarycalc -i -1 testout/summarycalc_S1_a1.bin -2 testout/summarycalc_S2_a1.bin -3 testout/summarycalc_S3_a1.bin
-	#summarycalctocsv -e < testout/summarycalc_S1_a1.bin > testout/gulsummarycalc_S1_a1_e.csv
-	#summarycalctocsv -e < testout/summarycalc_S2_a1.bin > testout/gulsummarycalc_S2_a1_e.csv
-	#summarycalctocsv -e < testout/summarycalc_S3_a1.bin > testout/gulsummarycalc_S3_a1_e.csv
 
 	# Test subperil allocation method a2 - Multiplicative sum and back-allocate to items in proportion
 	#eve 1 1 | getmodel | gulcalc -a2 -S10 -L0.1 -r -i - | gultocsv > testout/gulcalci_a2.csv
@@ -41,7 +41,7 @@ main()
 	   echo "Sorry check failed\n"
 	  exit 1
 	else
-	  echo "Installer test passed.\n"
+	  echo "Peril test passed.\n"
 	 exit 0
 	 fi
 	 rm *
