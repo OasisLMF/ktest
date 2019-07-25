9d86f310c8b8f36167b210833ac0d24ef5172099
hchagani-oasislmf <hassan.chagani@oasislmf.org>
2019-07-25 14:53:28 +0100

    Updated for validatefootprint

--- validationtest/runtests.sh
+++ validationtest/runtests.sh
@@ -5,11 +5,17 @@ main()
 	mkdir testout
 	CTRL=ctrl
 
-	# Loop over all csv files in static directory and create binary (bin)
-	# and stderr (err) files
-	for filename in static/*.csv; do
+	# Loop over all csv files in static directory and create stderr (err)
+	# files
+	# Damage bin dict
+	for filename in static/damage_bin_dict*.csv; do
 		base=$(basename -s .csv "$filename")
 		validatedamagebin < static/$base.csv 2>testout/$base.err
+	done
+	# Footprint
+	for filename in static/footprint*.csv; do
+		base=$(basename -s .csv "$filename")
+		validatefootprint < static/$base.csv 2>testout/$base.err
 	done
 
 	# Conduct MD5 checksum on generated files
