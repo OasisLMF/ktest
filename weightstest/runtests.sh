a6ce51f5797cc11bcd7eb0f5e0cd02ae73858337
joh <johanna.carter@oasislmf.org>
2020-03-13 13:03:15 +0000

    added test for weighted lec and comparison calc

--- weightstest/runtests.sh
+++ weightstest/runtests.sh
@@ -17,8 +17,9 @@ main()
 	
 	# test aalcalc
 	aalcalc -Kgul1/summarycalc > ../testout/a1_gulaal1.csv
-
-
+	# test leccalc
+	#leccalc -Kgul1/summarycalc -f ../testout/a1_gul_lec_f.csv -F ../testout/a1_gul_lec_F.csv -s ../testout/a1_gul_lec_s.csv -S ../testout/a1_gul_lec_S.csv -w ../testout/a1_gul_lec_w.csv -W ../testout/a1_gul_lec_W.csv -m ../testout/a1_gul_lec_m.csv -M ../testout/a1_gul_lec_M.csv  
+	 
 	# Test 2 - test with neutral period weights
 	cd ../a2
 	mkdir -p work/gul1/summarycalc
@@ -32,7 +33,8 @@ main()
 	eve -n 1 2 | getmodel | gulcalc -S100 -L0.1 -r -c - | summarycalc -g -1 work/gul1/summarycalc/p1.bin
 	eve -n 2 2 | getmodel | gulcalc -S100 -L0.1 -r -c - | summarycalc -g -1 work/gul1/summarycalc/p2.bin
 	aalcalc -Kgul1/summarycalc > ../testout/a3_gulaal1.csv
-
+	# test leccalc
+	leccalc -Kgul1/summarycalc -f ../testout/a3_gul_lec_f.csv -F ../testout/a3_gul_lec_F.csv -s ../testout/a3_gul_lec_s.csv -S ../testout/a3_gul_lec_S.csv -w ../testout/a3_gul_lec_w.csv -W ../testout/a3_gul_lec_W.csv -m ../testout/a3_gul_lec_m.csv -M ../testout/a3_gul_lec_M.csv  
 
 	cd ../testout
 	
