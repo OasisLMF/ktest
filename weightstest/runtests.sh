85a1fcda223dd228a61a1436a9f22e9b84573625
joh <johanna.carter@oasislmf.org>
2020-04-16 19:45:16 +0100

    updated weights tests

--- weightstest/runtests.sh
+++ weightstest/runtests.sh
@@ -18,14 +18,16 @@ main()
 	# test aalcalc
 	aalcalc -Kgul1/summarycalc > ../testout/a1_gulaal1.csv
 	# test leccalc
-	#leccalc -Kgul1/summarycalc -f ../testout/a1_gul_lec_f.csv -F ../testout/a1_gul_lec_F.csv -s ../testout/a1_gul_lec_s.csv -S ../testout/a1_gul_lec_S.csv -w ../testout/a1_gul_lec_w.csv -W ../testout/a1_gul_lec_W.csv -m ../testout/a1_gul_lec_m.csv -M ../testout/a1_gul_lec_M.csv  
-	 
+	leccalc -Kgul1/summarycalc -f ../testout/a1_gul_lec_f_occ.csv -F ../testout/a1_gul_lec_F_agg.csv -s ../testout/a1_gul_lec_s_occ.csv -S ../testout/a1_gul_lec_S_agg.csv -w ../testout/a1_gul_lec_w_occ.csv -W ../testout/a1_gul_lec_W_agg.csv -m ../testout/a1_gul_lec_m_occ.csv -M ../testout/a1_gul_lec_M_agg.csv  
+	#leccalc -Kgul1/summarycalc -r -f ../testout/a1_gul_lec_f_occ_r.csv -F ../testout/a1_gul_lec_F_agg_r.csv -s ../testout/a1_gul_lec_s_occ_r.csv -S ../testout/a1_gul_lec_S_agg_r.csv -w ../testout/a1_gul_lec_w_occ_r.csv -W ../testout/a1_gul_lec_W_agg_r.csv -m ../testout/a1_gul_lec_m_occ_r.csv -M ../testout/a1_gul_lec_M_agg_r.csv  
 	# Test 2 - test with neutral period weights
 	cd ../a2
 	mkdir -p work/gul1/summarycalc
 	eve -n 1 2 | getmodel | gulcalc -S100 -L0.1 -r -c - | summarycalc -g -1 work/gul1/summarycalc/p1.bin
 	eve -n 2 2 | getmodel | gulcalc -S100 -L0.1 -r -c - | summarycalc -g -1 work/gul1/summarycalc/p2.bin
 	aalcalc -Kgul1/summarycalc > ../testout/a2_gulaal1.csv
+	leccalc -Kgul1/summarycalc -f ../testout/a2_gul_lec_f_occ.csv -F ../testout/a2_gul_lec_F_agg.csv -s ../testout/a2_gul_lec_s_occ.csv -S ../testout/a2_gul_lec_S_agg.csv -w ../testout/a2_gul_lec_w_occ.csv -W ../testout/a2_gul_lec_W_agg.csv -m ../testout/a2_gul_lec_m_occ.csv -M ../testout/a2_gul_lec_M_agg.csv  
+	#leccalc -Kgul1/summarycalc -r -f ../testout/a2_gul_lec_f_occ_r.csv -F ../testout/a2_gul_lec_F_agg_r.csv -s ../testout/a2_gul_lec_s_occ_r.csv -S ../testout/a2_gul_lec_S_agg_r.csv -w ../testout/a2_gul_lec_w_occ_r.csv -W ../testout/a2_gul_lec_W_agg_r.csv -m ../testout/a2_gul_lec_m_occ_r.csv -M ../testout/a2_gul_lec_M_agg_r.csv  
 
 	# Test 3 - period 1 is double weight, period 2 is 0 weight
 	cd ../a3
@@ -35,6 +37,7 @@ main()
 	aalcalc -Kgul1/summarycalc > ../testout/a3_gulaal1.csv
 	# test leccalc
 	leccalc -Kgul1/summarycalc -f ../testout/a3_gul_lec_f_occ.csv -F ../testout/a3_gul_lec_F_agg.csv -s ../testout/a3_gul_lec_s_occ.csv -S ../testout/a3_gul_lec_S_agg.csv -w ../testout/a3_gul_lec_w_occ.csv -W ../testout/a3_gul_lec_W_agg.csv -m ../testout/a3_gul_lec_m_occ.csv -M ../testout/a3_gul_lec_M_agg.csv  
+	#leccalc -Kgul1/summarycalc -r -f ../testout/a3_gul_lec_f_occ_r.csv -F ../testout/a3_gul_lec_F_agg_r.csv -s ../testout/a3_gul_lec_s_occ_r.csv -S ../testout/a3_gul_lec_S_agg_r.csv -w ../testout/a3_gul_lec_w_occ_r.csv -W ../testout/a3_gul_lec_W_agg_r.csv -m ../testout/a3_gul_lec_m_occ_r.csv -M ../testout/a3_gul_lec_M_agg_r.csv  
 
 	cd ../testout
 	
@@ -44,7 +47,7 @@ main()
 	  echo "Sorry check failed\n"
 	#  exit 1
 	else
-	  echo "aalcalc test passed.\n"
+	  echo "weights test passed.\n"
 	# exit 0
 	fi
 	#rm *
