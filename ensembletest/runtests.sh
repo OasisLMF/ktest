ef0aae1d60acaa70f8a052ebd27d68908845c94d
joh <johanna.carter@oasislmf.org>
2022-11-16 17:03:49 +0000

    added lec test

--- ensembletest/runtests.sh
+++ ensembletest/runtests.sh
@@ -22,7 +22,7 @@ main()
 	aalcalc -Kgul1/summarycalc > ../testout/gulaal1.csv
 	# test leccalc
 #	leccalc -Kgul1/summarycalc -f ../testout/gul_lec_f_occ.csv -F ../testout/gul_lec_F_agg.csv -s ../testout/gul_lec_s_occ.csv -S ../testout/gul_lec_S_agg.csv -w ../testout/gul_lec_w_occ.csv -W ../testout/gul_lec_W_agg.csv -m ../testout/gul_lec_m_occ.csv -M ../testout/gul_lec_M_agg.csv  
-#	leccalc -Kgul1/summarycalc -r -f ../testout/gul_lec_f_occ_r.csv -F ../testout/gul_lec_F_agg_r.csv
+	leccalc -Kgul1/summarycalc -r -f ../testout/gul_lec_f_occ_r.csv -F ../testout/gul_lec_F_agg_r.csv
 
 # Test 2 - add periods file. 
 	cd ../period # period 1 is double weight, period 2 is 0 weight. same as a3
@@ -35,8 +35,8 @@ main()
 	# test aalcalc
 	aalcalc -Kgul1/summarycalc > ../testout/period_gulaal1.csv
 	# test leccalc
-#	leccalc -Kgul1/summarycalc -f ../testout/period_gul_lec_f_occ.csv -F ../testout/period_gul_lec_F_agg.csv -s ../testout/period_gul_lec_s_occ.csv -S ../testout/period_gul_lec_S_agg.csv -w ../testout/period_gul_lec_w_occ.csv -W ../testout/period_gul_lec_W_agg.csv -m ../testout/period_gul_lec_m_occ.csv -M ../testout/period_gul_lec_M_agg.csv  
-#	leccalc -Kgul1/summarycalc -r -f ../testout/period_gul_lec_f_occ_r.csv -F ../testout/period_gul_lec_F_agg_r.csv
+#	leccalc -Kgul1/summarycalc -f ../testout/period_gul_lec_f_occ.csv -F ../testout/period_gul_lec_F_agg.csv  -s ../testout/period_gul_lec_s_occ.csv -S ../testout/period_gul_lec_S_agg.csv -w ../testout/period_gul_lec_w_occ.csv -W ../testout/period_gul_lec_W_agg.csv -m ../testout/period_gul_lec_m_occ.csv -M ../testout/period_gul_lec_M_agg.csv  
+	leccalc -Kgul1/summarycalc -r -f ../testout/period_gul_lec_f_occ_r.csv -F ../testout/period_gul_lec_F_agg_r.csv
 	
 # Test 3 - add ensemble file
 
@@ -46,7 +46,7 @@ main()
 	eve -n 2 2 | getmodel | gulcalc -S100 -L0.1 -r -c - | summarycalc -g -1 work/gul1/summarycalc/p2.bin
 	aalcalc -Kgul1/summarycalc > ../testout/ensemble_gulaal1.csv
 #	leccalc -Kgul1/summarycalc -f ../testout/ensemble_gul_lec_f_occ.csv -F ../testout/ensemble_gul_lec_F_agg.csv -s ../testout/ensemble_gul_lec_s_occ.csv -S ../testout/ensemble_gul_lec_S_agg.csv -w ../testout/ensemble_gul_lec_w_occ.csv -W ../testout/ensemble_gul_lec_W_agg.csv -m ../testout/ensemble_gul_lec_m_occ.csv -M ../testout/ensemble_gul_lec_M_agg.csv  
-#	leccalc -Kgul1/summarycalc -r -f ../testout/ensemble_gul_lec_f_occ_r.csv -F ../testout/ensemble_gul_lec_F_agg_r.csv
+	leccalc -Kgul1/summarycalc -r -f ../testout/ensemble_gul_lec_f_occ_r.csv -F ../testout/ensemble_gul_lec_F_agg_r.csv
 
 	
 	cd ../testout
